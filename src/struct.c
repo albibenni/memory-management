75609cffbfdebe3cd591c1a704424681ab78e4c2
Alberto Benatti <albi.benni8@gmail.com>
2024-10-25 10:17:06 +0200

    struct init

---
+++ src/struct.c
@@ -0,0 +1,23 @@
+struct Coordinate {
+  int x;
+  int y;
+  int z;
+};
+
+struct Coordinate new_coord(int x, int y, int z) {
+  struct Coordinate coo = {
+    .x = x,
+    .y = y,
+    .z = z
+  };
+  return coo;
+}
+
+struct Coordinate new_coord(int x, int y, int z) {
+  struct Coordinate coo = {
+    .x = x,
+    .y = y,
+    .z = z
+  };
+  return coo;
+}
