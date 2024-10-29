ad940247241e7b1f76894c8965ddf4996328a87f
Alberto Benatti <albi.benni8@gmail.com>
2024-10-29 17:03:37 +0100

    dereference maybe understood

---
+++ src/pointer-deference.c
@@ -0,0 +1,13 @@
+codefile_t change_filetype(codefile_t *f, int new_filetype){
+  codefile_t * new_f = f;
+  new_f->filetype = new_filetype;
+  return *new_f;
+}
+
+// correct way no arrow?
+
+codefile_t change_filetype(codefile_t *f, int new_filetype){
+  codefile_t new_f = *f;
+  new_f.filetype = new_filetype;
+  return new_f;
+}
